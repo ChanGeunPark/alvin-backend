c1abe5f6c612ab4bc1930d16525bec1aff29a9d7
박찬근 <design795@naver.com>
2023-07-02 16:55:16 +0900

    NODE_ENV production

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,9 +27,10 @@ import { CharacterModule } from './character/character.module';
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
 
       // 서버에 deploy 할 때는 .env 파일을 사용하지 않는다.
-      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      ignoreEnvFile: process.env.NODE_ENV === 'production', // heroku에서는 production이라고 사용해야 한다.
       validationSchema: Joi.object({
         // 환경변수 유효성 검사
+        NODE_ENV: Joi.string().valid('dev', 'production', 'test').required(),
         DATABASE_URL: Joi.string().required(),
         PRIVATE_KEY: Joi.string().required(),
         MAIL_API_KEY: Joi.string().required(),
@@ -40,7 +41,7 @@ import { CharacterModule } from './character/character.module';
 
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      playground: false,
+      playground: process.env.NODE_ENV !== 'production', // 배포할 때는 playground를 false로 해야한다.
       autoSchemaFile: true,
       // autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       plugins: [ApolloServerPluginLandingPageLocalDefault()], // apollo server plugin -> ui를 제공해준다.
